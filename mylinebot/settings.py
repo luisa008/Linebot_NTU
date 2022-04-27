0c8a57daad738f00719413fdd2690126a96ee195
luisa008 <b08902008@ntu.edu.tw>
2022-04-27 22:37:44 +0800

    web_server19

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = str(BASE_DIR / 'static')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "mylinebot" / "static",
 ]
