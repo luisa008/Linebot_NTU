55250900dee1964740ad44b8016b0b9d03601991
luisa008 <b08902008@ntu.edu.tw>
2022-04-27 21:00:46 +0800

    web_server15

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "mylinebot" / "static",
 ]
 
 # Default primary key field type
