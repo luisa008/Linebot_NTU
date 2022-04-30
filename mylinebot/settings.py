79b3406cc74fc47e9458fe6fbb8ec388334e9c92
luisa008 <b08902008@ntu.edu.tw>
2022-04-30 13:31:31 +0800

    add hint to admin3

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -122,16 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static'
+STATIC_URL = 'static/'
 
 # When run with python manage.py runserver (development)
 if sys.argv[1] == 'runserver':
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'static')
+        os.path.join(BASE_DIR,'static/')
     ]
 # When deploy on Heroku (deploy)
 else:
-    STATIC_ROOT = 'static'
+    STATIC_ROOT = 'static/'
     
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
