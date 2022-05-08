ebe379d369ab7481902eca809a3caa68a018990d
luisa008 <b08902008@ntu.edu.tw>
2022-05-08 23:25:14 +0800

    try postgresql5

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import sys
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,26 +81,27 @@ WSGI_APPLICATION = 'mylinebot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# try:
-#     DATABASES['default'] = dj_database_url.config()
-# except NameError:
-#     pass
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'mydb',                     
-        'USER': 'myuser',
-        'PASSWORD': 'password',
-        'HOST': '',                    
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+try:
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
+except NameError:
+    pass
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mydb',                     
+#         'USER': 'myuser',
+#         'PASSWORD': 'password',
+#         'HOST': '',                    
+#         'PORT': '',                      # Set to empty string for default.
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
